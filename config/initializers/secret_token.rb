dcbb402257fa728989bb10cd3d9467b9a1c63de0
Ricardo Galvao <ricardo.galvao@callixbrasil.com>
2014-04-24 10:50:59 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TraceViewer::Application.config.secret_key_base = '5437a7a54c9b4f47fd3d69d33fab776efc457badf2b05f202f11cf2eb380ed6d727f0ea430399e5db694ec5dcb3c66073acb5c8b42882200df31bd2cf18dea09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TraceViewer::Application.config.session_store :cookie_store, key: '_trace_viewer_session'
